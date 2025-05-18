d45d5dce462826acc9e1e900356361f5484d053f
MaximaSim <sim240770@spengergasse.at>
2025-05-18 23:20:38 +0200

    Charaktere hinzugefügen

--- app/src/main/java/com/example/talismanv01/CharacterManager.java
+++ app/src/main/java/com/example/talismanv01/CharacterManager.java
@@ -11,10 +11,26 @@ public class CharacterManager {
     // Diese Methode fügt die Charaktere zur Liste hinzu
     static {
         allCharacters.add(new GameCharacter("Krieger", 4, 2, 1, 5, 3, "Base"));
-        allCharacters.add(new GameCharacter("Zauberer", 2, 5, 2, 4, 5, "Base"));
-        allCharacters.add(new GameCharacter("Wächter", 3, 3, 2, 6, 4, "City"));
-        allCharacters.add(new GameCharacter("Schurke", 4, 4, 1, 3, 3, "City"));
-        allCharacters.add(new GameCharacter("Druide", 3, 4, 3, 5, 2, "Forest"));
+        allCharacters.add(new GameCharacter("Magier", 2, 5, 1, 4, 3, "Base"));
+        allCharacters.add(new GameCharacter("Barde", 2, 4, 1, 4, 5, "Base"));
+        allCharacters.add(new GameCharacter("Assassine", 3, 3, 1, 4, 3, "Base"));
+        allCharacters.add(new GameCharacter("Zwerg", 3, 3, 1, 5, 5, "Base"));
+        allCharacters.add(new GameCharacter("Zauberin", 2, 4, 1, 4, 3, "Base"));
+        allCharacters.add(new GameCharacter("Dieb", 3, 3, 1, 4, 2, "Base"));
+        allCharacters.add(new GameCharacter("Wahrsagerin", 2, 4, 1, 4, 2, "Base"));
+        allCharacters.add(new GameCharacter("Troll", 6, 1, 1, 6, 1, "Base"));
+        allCharacters.add(new GameCharacter("Priester", 2, 4, 1, 4, 5, "Base"));
+        allCharacters.add(new GameCharacter("Mönch", 2, 3, 1, 4, 5, "Base"));
+        allCharacters.add(new GameCharacter("Elf", 3, 4, 1, 4, 3, "Base"));
+        allCharacters.add(new GameCharacter("Ghul", 2, 4, 1, 4, 4, "Base"));
+        allCharacters.add(new GameCharacter("Druide", 2, 4, 1, 4, 4, "Base"));
+
+        allCharacters.add(new GameCharacter("Kleine Fee", 1, 5, 1, 4, 1, "High"));
+        allCharacters.add(new GameCharacter("Vampirgräfin", 3, 3, 1, 5, 3, "High"));
+        allCharacters.add(new GameCharacter("Hochländer", 4, 2, 1, 4, 2, "High"));
+        allCharacters.add(new GameCharacter("Gaunerin", 3, 3, 1, 4, 4, "High"));
+        allCharacters.add(new GameCharacter("Walküre", 3, 4, 1, 4, 3, "High"));
+        allCharacters.add(new GameCharacter("Alchimist", 2, 4, 1, 4, 3, "High"));
     }
 
     // Methode, um alle Charaktere zurückzugeben
